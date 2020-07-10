ec41552d7b435ec1b9fc10452e0e58bd00345a9c
Fizvlad <fizvlad@mail.ru>
2020-07-10 09:54:00 +0300

    Project component

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+export interface Todo {
+  text: string
+  isCompleted: boolean
+}
+
+export interface Project {
+  title: string
+  todos: Todo[]
+}
 
 @Component({
-	selector: 'app-project',
+  selector: 'app-project',
   templateUrl: './project.component.html',
   styleUrls: ['./project.component.scss']
 })
-export class ProjectComponent { }
+export class ProjectComponent {
+  @Input() project: Project
+
+  OnTodoStatusChange(todo: Todo, project: Project) {
+    todo.isCompleted = !todo.isCompleted
+    // TODO: Send data to server or whatever
+  }
+}
