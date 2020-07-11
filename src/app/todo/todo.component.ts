62305611d376388e83aebc62e53fbd68354394f9
Fizvlad <fizvlad@mail.ru>
2020-07-11 13:34:26 +0300

    Make todo text input actually work

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,4 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit, EventEmitter, Input, Output } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+import 'reflect-metadata';
+import { Type, plainToClass } from 'class-transformer';
 
 import { Project } from '../project/project.component'
 
@@ -19,10 +23,58 @@ export class Todo {
 export class TodoComponent {
   @Input() todo: Todo
   @Input() project: Project
+  @Output() onCreateNewTodo = new EventEmitter();
+
+  newTodoReactiveForm: FormGroup;
+
+  constructor(private form_builder: FormBuilder) { }
+
+  ngOnInit() {
+    this.initForm();
+  }
 
   onTodoStatusChange(todo: Todo, project: Project) {
     console.debug('Toggling status of', todo, 'in', project);
     todo.toggleCompleted();
     // TODO: Send data to server or whatever
+  }
+
+  isControlInvalid(controlName: string): boolean {
+    let control = this.newTodoReactiveForm.controls[controlName];
+    return control.invalid && control.touched;
+  }
+
+  onKeyDown(event) {
+    if (event.keyCode === 13) {
+      this.onSubmit();
+    }
+  }
+
+  onSubmit() {
+    let controls = this.newTodoReactiveForm.controls;
+
+    // Mark all of controls as touched
+    if (this.newTodoReactiveForm.invalid) {
+      Object.keys(controls).forEach(controlName => controls[controlName].markAsTouched());
+      return;
+    }
+
+    let newTodo = new Todo(this.newTodoReactiveForm.value['text'], false);
+    this.onCreateNewTodo.emit(newTodo);
+
+    if (document.activeElement instanceof HTMLElement) {
+      document.activeElement.blur();
+    }
+
+    this.newTodoReactiveForm.reset();
+  }
+
+  private initForm() {
+    this.newTodoReactiveForm = this.form_builder.group({
+      text: [{value: '', disabled: this.project == null}, [
+          Validators.required
+        ]
+      ]
+    });
   }
 }
